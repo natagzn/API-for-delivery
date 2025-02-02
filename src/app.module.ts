a0c39e7b10ff1a2061abe548e64c8ce67a2f5c8c
nata <n221222122005@gmail.com>
2025-02-03 00:38:47 +0200

    creating a structure for the user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nest_db')
+    MongooseModule.forRoot('mongodb://localhost:27017/nest_db'),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
